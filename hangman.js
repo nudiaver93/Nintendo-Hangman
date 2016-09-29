3c6341a824976f24ba2253c76111f2b1f34cf843
Navin Udiaver <navinudiaver@wireless-10-146-176-13.public.utexas.edu>
2016-09-29 11:35:31 -0500

    Most HTML complete

---
+++ hangman.js
@@ -0,0 +1,20 @@
+var game = {
+
+	choices: {
+		"POKEMON RED AND BLUE" : {
+			image: 'red.png',
+			song: 'https://p.scdn.co/mp3-preview/add95157ad23fa4bf947378fea3c81efc7b0bd6b',
+		}
+
+		"OCARINA OF TIME" : {
+			image: 'ocarina.jpg',
+			song: 'https://p.scdn.co/mp3-preview/53eb65352e617c6e4fe381d9c840a04fbe0dc848',
+		}
+
+		"SUPER MARIO BROS" : {
+			image: 'mario.jpg',
+			song: 'https://p.scdn.co/mp3-preview/8fc64914f4a72d94e51f89032fac9d7f56c280bd',
+		}
+	}
+}
+
